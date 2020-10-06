9dc13ccc2b2214cae6a9f5f8a1b2d8eeb519c2a3
kimutomo7904 <kimutomo7904@gmail.com>
2020-10-06 23:29:06 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,236 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW > 880){
+		var headerH =20;
+	}
+	else{
+		var headerH =60;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+
+	$('.col4').onScreen({
+	   container: window,
+	   direction: 'vertical',
+	   doIn: function() {
+	     // 表示されたタイミングの処理
+	   },
+	   doOut: function() {
+	     // 非表示になったタイミングでの処理
+	   },
+	   tolerance: 0,
+	   throttle: 50,
+	   toggleClass: 'onScreen',
+	   lazyAttr: null,
+	   lazyPlaceholder: 'someImage.jpg',
+	   debug: false
+	});
+
+    $('.col4 li img')
+    .hover(
+        function(){
+            $(this).stop().animate({
+                'width':'130px',
+                'height':'130px',
+                'marginTop':'-32px'
+            },'fast');
+        },
+        function () {
+            $(this).stop().animate({
+                'width':'70px',
+                'height':'70px',
+                'marginTop':'0px'
+            },'fast');
+        }
+    );
+});
+
+$(document).ready(function () {
+	const element1 = document.getElementById("gal");
+	element1.addEventListener("click", () => {
+		$(document.body).fadeOut("fast", () => {});
+	});
+});
+
+$(document).ready(function(){
+  $('#tagcloud').tagoSphere({
+    height: 300,
+    width: 300,
+    radius: 150,
+    speed: 0.2,
+    slower: 0.9,
+    timer: 5,
+    fontMultiplier: 15
+  });
+});
+
+
+//分子
+particlesJS('hoge',{
+  "particles":{
+ 
+//--シェイプの設定----------
+      "number":{
+        "value":30, //シェイプの数
+        "density":{
+          "enable":true, //シェイプの密集度を変更するか否か
+          "value_area":100 //シェイプの密集度
+        }
+      },
+      "shape":{
+        "type":"circle", //シェイプの形（circle:丸、edge:四角、triangle:三角、polygon:多角形、star:星型、image:画像）
+        "stroke":{
+          "width":0, //シェイプの外線の太さ
+          "color":"#ffcc00" //シェイプの外線の色
+        },
+        //typeをpolygonにした時の設定
+        "polygon": {
+          "nb_sides": 5 //多角形の角の数
+        },
+        //typeをimageにした時の設定
+        "image": {
+          "src": "images/hoge.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "color":{
+        "value":"#ffffff" //シェイプの色
+      },
+      "opacity":{
+        "value":0.5, //シェイプの透明度
+        "random":false, //シェイプの透明度をランダムにするか否か
+        "anim":{
+          "enable":true, //シェイプの透明度をアニメーションさせるか否か
+          "speed":10, //アニメーションのスピード
+          "opacity_min":0.1, //透明度の最小値
+          "sync":false //全てのシェイプを同時にアニメーションさせるか否か
+        }
+      },
+      "size":{
+        "value":4, //シェイプの大きさ
+        "random":true, //シェイプの大きさをランダムにするか否か
+        "anim":{
+          "enable":false, //シェイプの大きさをアニメーションさせるか否か
+          "speed":40, //アニメーションのスピード
+          "size_min":0.1, //大きさの最小値
+          "sync":false //全てのシェイプを同時にアニメーションさせるか否か
+        }
+      },
+//--------------------
+
+//--線の設定----------
+      "line_linked":{
+        "enable":true, //線を表示するか否か
+        "distance":150, //線をつなぐシェイプの間隔
+        "color":"#ffffff", //線の色
+        "opacity":0.4, //線の透明度
+        "width":1 //線の太さ
+      },
+//--------------------
+
+//--動きの設定----------
+      "move":{
+        "speed":10, //シェイプの動くスピード
+        "straight":false, //個々のシェイプの動きを止めるか否か
+        "direction":"none", //エリア全体の動き(none、top、top-right、right、bottom-right、bottom、bottom-left、left、top-leftより選択)
+        "out_mode":"out" //エリア外に出たシェイプの動き(out、bounceより選択)
+      }
+//--------------------
+
+    },
+ 
+    "interactivity":{
+      "detect_on":"canvas",
+      "events":{
+
+//--マウスオーバー時の処理----------
+        "onhover":{
+          "enable":true, //マウスオーバーが有効か否か
+          "mode":"repulse" //マウスオーバー時に発動する動き(下記modes内のgrab、repulse、bubbleより選択)
+        },
+//--------------------
+
+//--クリック時の処理----------
+        "onclick":{
+          "enable":true, //クリックが有効か否か
+          "mode":"remove" //クリック時に発動する動き(下記modes内のgrab、repulse、bubble、push、removeより選択)
+        },
+//--------------------
+
+      },
+ 
+      "modes":{
+
+//--カーソルとシェイプの間に線が表示される----------
+        "grab":{
+          "distance":400, //カーソルからの反応距離
+          "line_linked":{
+            "opacity":1 //線の透明度
+          }
+        },
+//--------------------
+
+//--シェイプがカーソルから逃げる----------
+        "repulse":{
+          "distance":200 //カーソルからの反応距離
+        },
+//--------------------
+
+//--シェイプが膨らむ----------
+        "bubble":{
+          "distance":400, //カーソルからの反応距離
+          "size":40, //シェイプの膨らむ大きさ
+          "opacity":8, //膨らむシェイプの透明度
+          "duration":2, //膨らむシェイプの持続時間(onclick時のみ)
+          "speed":3 //膨らむシェイプの速度(onclick時のみ)
+        },
+//--------------------
+
+//--シェイプが増える----------
+        "push":{
+          "particles_nb":4 //増えるシェイプの数
+        },
+//--------------------
+
+//--シェイプが減る----------
+        "remove":{
+          "particles_nb":2 //減るシェイプの数
+        }
+//--------------------
+
+      }
+    },
+    "retina_detect":true, //Retina Displayを対応するか否か
+    "resize":true //canvasのサイズ変更にわせて拡大縮小するか否か
+  }
+);
