d74d4f4c330959af48cb7e17fb74a7ee06486640
kimutomo7904 <72303555+kimutomo7904@users.noreply.github.com>
2020-11-13 23:39:47 +0900

    Create main.js

---
+++ js/videos_js/main.js
@@ -0,0 +1,112 @@
+$(function(){
+	
+	var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+	
+	if (winW > 880){
+		var headerH =20;
+	}
+	else{
+		var headerH =60;
+	}
+	
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+	
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+});
+
+
+$(function(){
+		$('#slick-test').slick({
+		    autoplay:true,
+		    autoplaySpeed:4000,
+				arrows:false,
+		    dots:true,
+		    slidesToShow:4,
+		    responsive:[
+		        {
+		            breakpoint: 1024,
+		            settings:{
+		                slidesToShow:3,
+		            }
+		        },
+		        {
+		            breakpoint: 768,
+		            settings:{
+		                slidesToShow:2,
+		            }
+		        },
+		        {
+		            breakpoint: 480,
+		            settings:{
+		                slidesToShow:1,
+		            }
+		        },
+		    ]	
+		});
+  });
+
+$(function () {
+
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+
+
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });  
+});
+
+$(function(){
+	$(window).scroll(function (){
+		$('.wrapper').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 200){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+});
